149ccc171e970596aaa9b35534f283ff47cf8e78
HadjerDA <38032794+HadjerDA@users.noreply.github.com>
2018-11-22 12:34:21 +0100

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -19,7 +19,7 @@ public class Greeter {
   
   /**
   *@param someone name of a person
-  *return greeting string
+  *@return greeting string
   */
 
   public final String greet(final String someone) {
