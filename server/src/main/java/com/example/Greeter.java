c57f8dc2299227d23432d5d515277520fd5d6e2a
HadjerDA <38032794+HadjerDA@users.noreply.github.com>
2018-11-22 12:42:44 +0100

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -1,21 +1,13 @@
-/**
-* Javadoc du package
-*/
-
 package com.example;
 
 /**
 * This is a class
 */
-
-
 public class Greeter {
   
   /**
     * This is a constructor.
     */
-
-  
   public Greeter() {
    
   }
@@ -24,7 +16,6 @@ public class Greeter {
   *@param someone name of a person
   *@return greeting string
   */
-
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
